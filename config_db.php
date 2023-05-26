81e10f43df78e7f4991e3f416afb0973af095799
Wagner Leandro <wagnerleandro3060@gmail.com>
2023-05-25 22:53:57 -0300

    Update config_db.php
    adicionado a porta do railway

--- config_db.php
+++ config_db.php
@@ -1,13 +1,15 @@
 <?php
 
-$server_name = 'containers-us-west-84.railway.app';
+$server_name = 'containers-us-west-84.railway.app:7544';
 $user_name = 'root';
 $password = '9LPyrf8Xq2TorNuwmsoP';
 $db_name = 'railway';
 
 $connection = mysqli_connect($server_name, $user_name, $password, $db_name);
 $connection->set_charset('utf8');
-echo mysqli_connect_errno();
+
+
 if (!$connection) {
     die("FALHA NA CONEXÃO ". mysqli_connect_error());
 }
+echo "Conectado!!!";
